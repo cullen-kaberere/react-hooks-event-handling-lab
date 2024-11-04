f2cc04fa75e366194e89b1aa45b4e8724f41c3ef
cullen kaberere <kabesh86@gmail.com>
2024-11-04 08:11:47 +0300

    Add EyeOnMe js

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,29 @@
 // Code EyesOnMe Component Here
+// Code EyesOnMe Component Here
+import React,{useState} from "react";
+
+function EyesOnMe (){
+    const [isFocused,isSetFocused] = useState('false');
+    
+
+    const handleFocus = () => {
+        isSetFocused(true);
+        console.log('Good!');
+    };
+    const handleBlur = () => {
+        isSetFocused(false);
+        console.log('Hey! Eyes on me!')
+    }
+    return( 
+        <>
+        <button 
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        >
+            Eyes on me</button>
+         </>)
+         
+   
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,24 @@
 // Code Keypad Component Here
+import React, {useState} from "react";
 
 function Keypad (){
+    const [password, setPassword] = useState('');
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+        console.log('Entering password...');
+    };
     return (
-        <div></div>
-    )
+        <div>
+            <input 
+            type="password" 
+            value={password}
+            onChange={handlePasswordChange} 
+            placeholder="Enter password"
+            />
+           
+        </div>
+    );
 }
 
 export default Keypad;
